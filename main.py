8dc96b0071c95abd7b64645e6b2907449b155049
Radoslaw Mierzwa <chalkenteros@gmail.com>
2022-01-11 21:18:29 +0100

    Add spinningup HalfCheetah-v2 ppo grid search

---
+++ main.py
@@ -0,0 +1,20 @@
+from spinup.utils.run_utils import ExperimentGrid
+from spinup import ppo_pytorch
+import torch
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cpu', type=int, default=4)
+    parser.add_argument('--num_runs', type=int, default=3)
+    parser.add_argument('--name', type=str, default='ppo-pyt-half-cheetah')
+    args = parser.parse_args()
+
+    eg = ExperimentGrid(name=args.name)
+    eg.add('env_name', 'HalfCheetah-v2', '', True)
+    eg.add('seed', [10*i for i in range(args.num_runs)])
+    eg.add('epochs', 10)
+    eg.add('steps_per_epoch', 4000)
+    eg.add('ac_kwargs:hidden_sizes', [(32,), (64,64)], 'hid')
+    eg.add('ac_kwargs:activation', [torch.nn.Tanh, torch.nn.ReLU], '')
+    eg.run(ppo_pytorch, num_cpu=args.cpu)
