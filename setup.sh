85beb708507bac628a205c39b57d21ddbc6f7976
Radoslaw Mierzwa <01133376@pw.edu.pl>
2022-01-14 10:31:50 +0100

    Add setup script to prepare experiments environment

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#prepare venv
+python -m venv usd-ppo
+source usd-ppo/bin/activate
+
+#install spinup
+git clone https://github.com/openai/spinningup.git
+git checkout 038665d62d569055401d91856abb287263096178
+cd spinningup
+pip install -e .
+cd ../
+
+#install ray rllib
+pip install "ray[rllib]" tensorflow torch
+
+#install mujoco
+wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
+tar -xzvf ./mujoco210-linux-x86_64.tar.gz
+rm mujoco210-linux-x86_64.tar.gz
+mv ./mujoco210 ~/.mujoco
+pip install 'mujoco-py<2.2,>=2.1'
